42d54b90688b49c5af8282f841f3a41a7d0ec7b3
vorop <2vvoropaevv2@gmail.com>
2023-10-16 00:12:11 +0700

    Compute average prediction

--- Regression/ElasticNet.py
+++ Regression/ElasticNet.py
@@ -1,4 +1,6 @@
 from copy import deepcopy
+
+import numpy as np
 from sklearn.impute import SimpleImputer
 from sklearn.linear_model import ElasticNetCV
 from sklearn.model_selection import train_test_split
@@ -6,10 +8,6 @@ from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
 from Utils import *
-
-
-class StandartScaler:
-    pass
 
 
 def start_point():
@@ -23,23 +21,29 @@ def start_point():
             md_df = md_df[md_df["type"] == win_col[i]]
         x = md_df.drop(columns='quality')
         y = md_df["quality"]
+        print(f"Iteration: {i}")
+        prepare_to_avg: list = []
 
-        x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, stratify=y)
+        for j in range(10):
+            x_train, x_test, y_train, y_test = train_test_split(x, y, train_size=0.7, stratify=y)
 
-        median = SimpleImputer(strategy='median')
-        scaler = StandardScaler()
-        num_pipe = make_pipeline(median, scaler)
-        x_train = num_pipe.fit_transform(x_train)
-        x_test = num_pipe.fit_transform(x_test)
+            median = SimpleImputer(missing_values=-1, strategy='median')
+            scaler = StandardScaler()
+            num_pipe = make_pipeline(median, scaler)
+            x_train = num_pipe.fit_transform(x_train)
+            x_test = num_pipe.fit_transform(x_test)
 
-        reg = ElasticNetCV(cv=10, random_state=42, n_jobs=5)
-        reg.fit(x_train, np.ravel(y_train))
-        y_test = np.ravel(y_test)
-        y_pred = reg.predict(x_test)
-        success = 0
-        for ind in range(len(y_pred)):
-            y = y_test[ind]
-            yp = y_pred[ind]
-            if y - yp < 1:
-                success += 1
-        print(success / len(x_test) * 100)
+            reg = ElasticNetCV(cv=10, n_jobs=5)
+            reg.fit(x_train, y_train)
+            y_test = np.ravel(y_test)
+            y_pred = reg.predict(x_test)
+            success = 0
+            for ind in range(len(y_pred)):
+                y_data = y_test[ind]
+                yp = y_pred[ind]
+                if np.abs(y_data - yp) < 1:
+                    success += 1
+            prepare_to_avg.append(success / len(y_test) * 100)
+            print(prepare_to_avg[j])
+        avg = np.average(prepare_to_avg)
+        print(f"Average: {avg}")
